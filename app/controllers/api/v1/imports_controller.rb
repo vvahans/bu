82bac8f4978bbf488f48a341df70b4e018c9dc96
vvahans <vvahans@gmail.com>
2022-04-11 13:44:36 +0400

    Adding file upload
    - refactored create method to add file upload

--- app/controllers/api/v1/imports_controller.rb
+++ app/controllers/api/v1/imports_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::ImportsController < ApplicationController
   def create
+    @import = current_admin.imports.create!(file: permitted_params[:file])
+
+    render json: @import, status: :created
+  end
+
+  private
+
+  def permitted_params
+    params.require(:import).permit(:file)
   end
 end
