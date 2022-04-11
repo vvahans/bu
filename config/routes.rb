bd12a8210692198c71a055cb41bda7b16543c5a0
vvahans <vvahans@gmail.com>
2022-04-11 11:29:34 +0400

    Adding import endpoint
    - added imports controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope :api, module: 'api' do
+    scope :v1, module: 'v1' do
+      resource :import, only: [:create]
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
